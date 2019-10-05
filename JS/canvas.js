eb3b2444794bd6a2e2b56a7429449a5f8dc2c37c
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-10-05 10:17:14 +0100

    initial class setup

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,5 +1,41 @@
 class PulseHandler {
-    constructor()
+    constructor(startingX, startingY) {
+        this.startingX = startingX;
+        this.startingY = startingY;
+        this.pulses = [];
+        this.nextPulseId = 0;
+        this.nextPulseTimer = setTimeout(() => {
+            this.spawnPulse();
+        }, 1000);
+    }
+
+    spawnPulse(color) {
+        let color = color != undefined ? color : 'hsl(0, 0%, 25%)';
+
+
+    }
+}
+
+class Pulse {
+    constructor(originX, originY, color, radialStep, verticiesCount) {
+        this.originX = originY;
+        this.originY = originX;
+        this.color = color;
+        this.radialStep = radialStep;
+        this.verticies = [];
+
+        for(let i = 0; i < verticiesCount; i++) {
+            this.verticies.push(new Verticy());
+        }
+    }
+}
+
+class Verticy {
+    constructor(positionDifference) {
+        this.prevPosition = positionDifference;
+        this.currentPosition = positionDifference;
+        this.nextPosition;
+    }
 }
 
 const canvas = document.querySelector('#background');
